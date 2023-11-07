730d3927c1444de37d006769d3d85843942900bf
Sivareddy0930 <sivareddy0930@gmail.com>
2023-11-07 18:14:46 +0530

    vasu

--- src/main/java/com/siva/FirstSpringBootAppApplication.java
+++ src/main/java/com/siva/FirstSpringBootAppApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstSpringBootAppApplication {
+public class FirstSpringBootAppApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(FirstSpringBootAppApplication.class, args);
+		System.out.println("/ Vasu is Back!");
 	}
 
 }
